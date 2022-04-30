5525c4116ad5987484cf3d205093d6978d568e3c
nathan-michel-devoteam <102220699+nathan-michel-devoteam@users.noreply.github.com>
2022-04-30 19:59:45 +0200

    Update settings.py

--- crypto_carbon_footprint/settings.py
+++ crypto_carbon_footprint/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-fej$*qxexg-kfqn4&n5go%&-$x$a#iss1&(lzz0)9x7z#5tr$4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -117,9 +117,11 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
+
