c5eba13a1c8f86d77da9dadfb43d5fa15546cad4
ayoub-hafidi-alaoui <ayoub.hafidi.alaoui@hotmail.fr>
2022-04-30 13:17:36 +0200

    add model

--- crypto_app/models.py
+++ crypto_app/models.py
@@ -1,11 +1,16 @@
 # Create your models here.
+from django.contrib.auth.models import User
 from django.db import models
-from django.template.defaulttags import now
 
 
 class CryptoCurrency(models.Model):
     name = models.CharField(max_length=30, null=True)
     carbon_foot_print = models.FloatField(null=True)
 
+    def __str__(self):
+        return self.name
 
+
+class Transaction(models.Model):
+    name = models.FloatField(max_length=30, null=True)
 
